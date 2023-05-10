1684de317e6709feb7b8077b22953aa21ac827b8
Bruna Terribile <brunaterribile@gmail.com>
2023-05-10 16:23:17 -0300

    feat: payment page, basic ticket + paym components

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,19 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+import useTicket from '../../../hooks/api/useTicket';
+import TicketArea from '../../../components/TicketArea';
+import PaymentArea from '../../../components/PaymentArea';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const { ticket } = useTicket();
+  return (
+    <>
+      <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
+      {ticket? <PaymentArea /> : <TicketArea />}
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
