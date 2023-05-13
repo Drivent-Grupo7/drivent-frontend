f525b389caf92303cff8130c498205242003b0b7
Bruna Terribile <brunaterribile@gmail.com>
2023-05-13 16:31:16 -0300

    feat: delete console.log + use context to payment

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 import useLocalStorage from '../hooks/useLocalStorage';
 
@@ -7,9 +7,10 @@ export default UserContext;
 
 export function UserProvider({ children }) {
   const [userData, setUserData] = useLocalStorage('userData', {});
+  const [ confirmedPayment, setConfirmedPayment ] = useState(false);
   
   return (
-    <UserContext.Provider value={{ userData, setUserData }}>
+    <UserContext.Provider value={{ userData, setUserData, confirmedPayment, setConfirmedPayment }}>
       {children}
     </UserContext.Provider>
   );
