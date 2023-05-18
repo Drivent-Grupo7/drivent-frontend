4502e965e5f5c666fbee77038d31bc3835de016d
igor-ribeir0 <igor_ribeirosantos@outlook.com>
2023-05-18 20:08:23 -0300

    feat: filtering activities by day

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -4,23 +4,66 @@ import styled from 'styled-components';
 
 export default function Activities() {
   const { ticket } = useTicket();
-  const [ message, setMessage ] = useState('Em breve!');
+  const [message, setMessage] = useState('');
+  const [reservedTicket, setReservedTicket] = useState(true);
+  const [selectedButton, setSelectedButton] = useState(null);
 
   useEffect(() => {
-    if(ticket) {
-      if(ticket.status === 'RESERVED') {
-        setMessage('Você precisa ter confirmado pagamento antes de fazer a escolha de atividades');
+    if (ticket) {
+      if (ticket.TicketType.isRemote) {
+        setMessage(
+          'Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades'
+        );
       }
-      if(ticket.TicketType.isRemote) {
-        setMessage('Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades');
+
+      if (ticket.status === 'RESERVED') {
+        setMessage('Você precisa ter confirmado pagamento antes de fazer a escolha de atividades');
+      } else {
+        setReservedTicket(false);
+        setMessage('');
       }
     }
   }, [ticket, setMessage]);
+
+  function SelectDay(event) {
+    const index = parseInt(event.target.dataset.index);
+    setSelectedButton(index);
+  }
 
   return (
     <Container>
       <Titulo>Escolha de atividades</Titulo>
       <Erro>{message}</Erro>
+
+      <Conteudo reservedTicket={reservedTicket}>
+        <SubTitulo>Primeiro, filtre pelo dia do evento:</SubTitulo>
+
+        <CaixaBotoes>
+          <button
+            onClick={SelectDay}
+            data-index={0}
+            style={{ backgroundColor: selectedButton === 0 ? 'orange' : '#E0E0E0' }}
+          >
+            Sexta, 22/10
+          </button>
+
+          <button
+            onClick={SelectDay}
+            data-index={1}
+            style={{ backgroundColor: selectedButton === 1 ? 'orange' : '#E0E0E0' }}
+          >
+            Sábado, 23/10
+          </button>
+
+          <button
+            onClick={SelectDay}
+            data-index={2}
+            style={{ backgroundColor: selectedButton === 2 ? 'orange' : '#E0E0E0' }}
+          >
+            Domingo, 24/10
+          </button>
+        </CaixaBotoes>
+      </Conteudo>
     </Container>
   );
 }
@@ -30,12 +73,12 @@ const Container = styled.div`
   flex-direction: column;
   height: 100%;
   width: 100%;
-  h1{
+  h1 {
     margin-top: 36px;
     margin-bottom: 18px;
     font-size: 20px;
     line-height: 23px;
-    color: #8E8E8E;
+    color: #8e8e8e;
   }
 `;
 
@@ -45,14 +88,57 @@ const Titulo = styled.div`
   color: #000000;
 `;
 
+const Conteudo = styled.div`
+  display: ${(props) => (props.reservedTicket ? 'none' : 'flex')};
+  flex-direction: column;
+  width: 100%;
+`;
+
+const SubTitulo = styled.div`
+  display: flex;
+  font-size: 20px;
+  line-height: 23px;
+  text-align: center;
+  color: #8e8e8e;
+  position: relative;
+  top: -490px;
+  margin-bottom: 23px;
+  width: 60%;
+`;
+
+const CaixaBotoes = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  button {
+    width: 131px;
+    height: 37px;
+    border-radius: 4px;
+    background-color: #e0e0e0;
+    align-items: center;
+    text-align: center;
+    border: none;
+    margin-right: 17px;
+    margin-top: -890px;
+  }
+  p {
+    width: 79px;
+    height: 16px;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    color: #000000;
+  }
+`;
+
 const Erro = styled.div`
   font-size: 20px;
   line-height: 23px;
   text-align: center;
-  color: #8E8E8E;
+  color: #8e8e8e;
   margin-top: auto;
   margin-bottom: auto;
   align-self: center;
   width: 60%;
 `;
-
